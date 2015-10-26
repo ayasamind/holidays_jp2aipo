5f5ab2b854b03ea836fe5cfe79b80adc40b518dc
[ayasamind] <[masayaliketennis@yahoo.co.jp]>
2015-10-26 05:17:00 +0100

    second commit

---
+++ change.php
@@ -0,0 +1,18 @@
+<?php
+
+$data = file_get_contents("php://stdin");
+$array = yaml_parse($data);
+$fp = fopen("holidays.properties", 'w');
+foreach($array as $key => $value){
+
+  $format = "%s,%s\n";
+  $hoge = sprintf($format, $value["name"], $value["date"]);
+  echo $hoge;
+//fwrite($fp, $value["name"]);
+//fwrite($fp, $value["date"]."\n"); 
+
+  fwrite($fp, $hoge);
+}
+fclose($fp);
+?>
+
