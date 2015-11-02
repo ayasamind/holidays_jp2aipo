7658ab48ac4ad248e8114694ba0fd009fda5af11
ayasamind <masayaliketennis@yahoo.co.jp>
2015-11-02 09:40:19 +0900

    Update change.php

--- change.php
+++ change.php
@@ -5,16 +5,12 @@ $data = file_get_contents("php://stdin");
 $array = yaml_parse($data);
 
 
-
-$fp = fopen("holidays_default.properties", 'w');
 foreach($array as $key => $value){
 
   $format = "%s,%s\n";
   $hoge = sprintf($format, $value["name"], $value["date"]);
   echo $hoge;
-
-  fwrite($fp, $hoge);
 }
-//fclose($fp);
+
 ?>
 
