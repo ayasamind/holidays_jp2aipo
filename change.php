59d91e6a99c6f2a9d61a764187545289bb56c9f8
[ayasamind] <[masayaliketennis@yahoo.co.jp]>
2015-11-02 09:20:00 +0900

    delete write

--- change.php
+++ change.php
@@ -6,15 +6,13 @@ $array = yaml_parse($data);
 
 
 
-$fp = fopen("holidays_default.properties", 'w');
 foreach($array as $key => $value){
 
   $format = "%s,%s\n";
   $hoge = sprintf($format, $value["name"], $value["date"]);
   echo $hoge;
 
-  fwrite($fp, $hoge);
 }
-//fclose($fp);
+
 ?>
 
