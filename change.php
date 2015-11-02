ea0ac8499a31b939f92fc0f8999bc2f3f27a4a6c
[ayasamind] <[masayaliketennis@yahoo.co.jp]>
2015-11-02 12:02:51 +0900

    change

--- change.php
+++ change.php
@@ -4,20 +4,12 @@
 $data = file_get_contents("php://stdin");
 $array = yaml_parse($data);
 
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 7658ab48ac4ad248e8114694ba0fd009fda5af11
 foreach($array as $key => $value){
 
   $format = "%s,%s\n";
   $hoge = sprintf($format, $value["name"], $value["date"]);
   echo $hoge;
-<<<<<<< HEAD
 
-=======
->>>>>>> 7658ab48ac4ad248e8114694ba0fd009fda5af11
 }
 
 ?>
